16c0220c7e0fbbb3f355dc078fa5cb4bd5fe58ca
Shintani Teppei <teppest1215@gmail.com>
2018-11-25 20:32:12 +0900

    Add style props

--- src/index.js
+++ src/index.js
@@ -1,46 +1,38 @@
 /**
  * @class ReactPdfJs
  */
-import PdfJsLib from 'pdfjs-dist';
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import PdfJsLib from "pdfjs-dist";
+import PropTypes from "prop-types";
+import React, { Component } from "react";
 
 export default class ReactPdfJs extends Component {
   static propTypes = {
-    file: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.object,
-    ]).isRequired,
+    file: PropTypes.oneOfType([PropTypes.string, PropTypes.object]).isRequired,
     page: PropTypes.number,
     onDocumentComplete: PropTypes.func,
     scale: PropTypes.number,
     cMapUrl: PropTypes.string,
     cMapPacked: PropTypes.bool,
+    style: PropTypes.string.isRequired,
     className: PropTypes.string,
-  }
+  };
 
   static defaultProps = {
     page: 1,
     onDocumentComplete: null,
     scale: 1,
-    cMapUrl: '../node_modules/pdfjs-dist/cmaps/',
+    cMapUrl: "../node_modules/pdfjs-dist/cmaps/",
     cMapPacked: false,
-  }
+  };
 
   state = {
     pdf: null,
   };
 
   componentDidMount() {
-    const {
-      file,
-      onDocumentComplete,
-      page,
-      cMapUrl,
-      cMapPacked,
-    } = this.props;
-    PdfJsLib.GlobalWorkerOptions.workerSrc = '//cdnjs.cloudflare.com/ajax/libs/pdf.js/2.0.943/pdf.worker.js';
-    PdfJsLib.getDocument({ url: file, cMapUrl, cMapPacked }).then((pdf) => {
+    const { file, onDocumentComplete, page, cMapUrl, cMapPacked } = this.props;
+    PdfJsLib.GlobalWorkerOptions.workerSrc = "//cdnjs.cloudflare.com/ajax/libs/pdf.js/2.0.943/pdf.worker.js";
+    PdfJsLib.getDocument({ url: file, cMapUrl, cMapPacked }).then(pdf => {
       this.setState({ pdf });
       if (onDocumentComplete) {
         onDocumentComplete(pdf._pdfInfo.numPages); // eslint-disable-line
@@ -60,22 +52,28 @@ export default class ReactPdfJs extends Component {
     }
   }
 
-  drawPDF = (page) => {
+  drawPDF = page => {
     const { scale } = this.props;
     const viewport = page.getViewport(scale);
     const { canvas } = this;
-    const canvasContext = canvas.getContext('2d');
-    canvas.height = viewport.height;
-    canvas.width = viewport.width;
+    const canvasContext = canvas.getContext("2d");
     const renderContext = {
       canvasContext,
       viewport,
     };
     page.render(renderContext);
-  }
+  };
 
   render() {
-    const { className } = this.props;
-    return <canvas ref={(canvas) => { this.canvas = canvas; }} className={className} />;
+    const { className, style } = this.props;
+    return (
+      <canvas
+        ref={canvas => {
+          this.canvas = canvas;
+        }}
+        style={style}
+        className={className}
+      />
+    );
   }
 }
